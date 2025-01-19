0d2b786c9f7a7a980b21a248ccb409e7c505fcdd
Juankrc <jcrodriguez488@gmail.com>
2025-01-19 00:39:38 -0500

    v3

--- script.js
+++ script.js
@@ -24,4 +24,13 @@
       carousel.style.transform = `translateX(-${currentIndex * cardTotalWidth}px)`;
     }
   });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+  
+    menuToggle.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  });
   
